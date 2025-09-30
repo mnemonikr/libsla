accf84285c2e3e08f056aba5f8eccce163a26cf3
mnemonikr <138624285+mnemonikr@users.noreply.github.com>
2025-09-30 14:37:30 -0700

    Remove XML SLA decoding from GhidraSleighBuilder (#6)
    The XML data passed down into libsla-sys is a wrapper that directs the
    underlying libsla library to decode a `.sla` file. In addition to being
    an unnecessary trampoline with no value, it limits the interface by
    requiring that the sla data reside in a file on disk. Maintaining this
    behavior provides no value and as such is being removed.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 //! This crate includes Rust bindings to the
-//! [Ghidra](https://github.com/NationalSecurityAgency/ghidra) Sleigh library libsla for translating
+//! [Ghidra](https://github.com/NationalSecurityAgency/ghidra) SLEIGH library libsla for translating
 //! native code to p-code.
 //!
-//! Sleigh is a processor specification language developed for the Ghidra used to describe
+//! SLEIGH is a processor specification language developed for the Ghidra used to describe
 //! microprocessors with enough detail to facilitate disassembly and decompilation. The
 //! processor-specific instructions are translated to **p-code**, which captures the instruction
 //! semantics independent of the specific processor. The details on how to perform this translation
-//! are captured by the compiled Sleigh specification for the processor.
+//! are captured by the compiled SLEIGH specification for the processor.
 
 mod opcodes;
 mod sleigh;
